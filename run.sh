685bfdfcecd95ab5ec12f0baab3bda83d0ee4367
tanjiro23 <70393102+tanjiro23@users.noreply.github.com>
2022-04-07 16:18:18 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-cp ./vfs/link.c /usr/src/minix/minix/servers/vfs/link.c;
-cp ./vfs/open.c /usr/src/minix/minix/servers/vfs/open.c;
-cp ./vfs/read.c /usr/src/minix/minix/servers/vfs/read.c;
-cp ./mfs/read.c /usr/src/minix/minix/fs/mfs/read.c;
-cp ./mfs/write.c /usr/src/minix/minix/fs/mfs/write.c;
-cp ./fslib.c /usr/src/minix/minix/lib/libc/gen/fslib.c;
-cp ./const.h /usr/src/minix/minix/include/minix/const.h;
-cd /usr/src/minix;
-make build MKUPDATE=yes >log.txt 2>log.txt;
